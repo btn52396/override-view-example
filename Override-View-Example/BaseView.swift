b1e53876a5dce31a92fa494012fead03a9a14433
Bryan Nguyen <bryannguyen@Bryans-MBP-2.attlocal.net>
2021-02-20 18:29:10 -0800

    first commit

---
+++ Override-View-Example/BaseView.swift
@@ -0,0 +1,8 @@
+//
+//  BaseView.swift
+//  Override-View-Example
+//
+//  Created by Bryan Nguyen on 2/20/21.
+//
+
+import Foundation
---
+++ Override-View-Example/BaseView2.swift
@@ -0,0 +1,8 @@
+//
+//  BaseView2.swift
+//  Override-View-Example
+//
+//  Created by Bryan Nguyen on 2/20/21.
+//
+
+import Foundation
